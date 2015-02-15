002cab2949f78fbde336247d31a59c73a6c66420
susanqin <yanqindb@gmail.com>
2015-02-15 17:35:13 -0500

    fourth

--- DBProj/src/HelloWord.java
+++ DBProj/src/HelloWord.java
@@ -5,8 +5,7 @@ public class HelloWord {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
-		System.out.println("Hello World!");
-		System.out.println("Hello World!");
+		
 	}
 
 }
