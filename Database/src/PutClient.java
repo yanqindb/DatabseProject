038429a7568da671cdaf03266ca88b0b61ed905a
Yan <ywang14@wpi.edu>
2015-02-18 23:52:41 -0500

    Project 1

---
+++ Database/src/PutClient.java
@@ -0,0 +1,26 @@
+/**
+ * Thread to put tuple
+ * @author Yan Wang & Zishan Qin
+ *
+ */
+public class PutClient implements Runnable{
+	   private Thread putThread;
+	   private String threadName;
+	   private ValueStore valueStore;
+	   private int key;
+	   private byte [] value;
+	   
+	   PutClient( String name, ValueStore valueStore, int key, byte [] value){
+	       threadName = name;
+	       this.valueStore = valueStore;
+	       this.key = key;
+	       this.value = value;
+	       putThread = new Thread (this, threadName);
+	       putThread.start ();
+
+	   }
+	   public void run() {
+        	valueStore.put(key, value);
+	   }
+
+}
